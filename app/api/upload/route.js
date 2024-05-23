dca150e04e391f6eb0728017b0cc5843907ebe56
John <64689339+john-ryabikov@users.noreply.github.com>
2024-05-23 16:52:16 +0300

    v. 0.3.8

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { put } from '@vercel/blob';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req) {
 
     const { searchParams } = new URL(req.url);
