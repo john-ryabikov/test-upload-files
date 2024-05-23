0093e3212c9753a5075f68c9a8bf6ec7df294310
John <64689339+john-ryabikov@users.noreply.github.com>
2024-05-23 16:33:40 +0300

    v. 0.3.2

--- components/GalleryImages.jsx
+++ components/GalleryImages.jsx
@@ -2,13 +2,13 @@ import Images from "./Images"
 
 export default async function GalleryImages() {
 
-    let BASE_URL = "http://localhost:3000"
+    // let BASE_URL = "http://localhost:3000"
 
-    if (process.env.VERCEL_ENV === "production") {
-        BASE_URL = "https://test-upload-files.vercel.app"
-    }
+    // if (process.env.VERCEL_ENV === "production") {
+    //     BASE_URL = "https://test-upload-files.vercel.app"
+    // }
 
-    const blob = await fetch(BASE_URL + "/api/images", {
+    const blob = await fetch("https://test-upload-files.vercel.app/api/images", {
         cache: "no-store"
     })
 
