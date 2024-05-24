0ef345eedf31ff580c83d13d649f7b307d380640
john-ryabikov <zhenya.ryabikov@mail.ru>
2024-05-24 09:42:16 +0300

    v. 0.4.0

--- components/GalleryImages.jsx
+++ components/GalleryImages.jsx
@@ -2,13 +2,13 @@ import Images from "./Images"
 
 export default async function GalleryImages() {
 
-    // let BASE_URL = "http://localhost:3000"
+    let BASE_URL = "https://test-upload-files.vercel.app"
 
-    // if (process.env.VERCEL_ENV === "production") {
-    //     BASE_URL = "https://test-upload-files.vercel.app"
-    // }
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "development") {
+        BASE_URL = "http://localhost:3000"
+    }
 
-    const blob = await fetch("https://test-upload-files.vercel.app/api/images", {
+    const blob = await fetch(BASE_URL + "/api/images", {
         cache: "no-cache"
     })
 
